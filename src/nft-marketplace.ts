758083c41119b240ff19eb463db6cb0a260e3015
Behrooz Payoon <Behrooz.payoon@gmail.com>
2022-06-17 12:19:39 +0430

    Init commit

---
+++ src/nft-marketplace.ts
@@ -0,0 +1,12 @@
+import {
+  ItemBought as ItemBoughtEvent,
+  ItemCancelled as ItemCancelledEvent,
+  ItemListed as ItemListedEvent
+} from "../generated/NftMarketplace/NftMarketplace"
+
+export function handleItemBought(event: ItemBoughtEvent): void {
+}
+
+export function handleItemCancelled(event: ItemCancelledEvent): void {}
+
+export function handleItemListed(event: ItemListedEvent): void {}
